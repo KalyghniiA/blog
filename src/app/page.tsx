ad1978dfa3441aecc33f074bd8cf37e42d4b197c
Александр <kalyghnii@gmail.com>
2026-01-13 12:23:58 +0500

    start

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
-export const metadata = {
-  title: "Мой блог",
-  description: "Это мой блог"
-}
-
 export default function Home() {
 
   return (

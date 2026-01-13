60257739faa78128447c187bf31d8a8c32b0af89
Александр <kalyghnii@gmail.com>
2026-01-13 12:21:14 +0500

    start

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
+export const metadata = {
+  title: "Мой блог",
+  description: "Это мой блог"
+}
+
 export default function Home() {
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
